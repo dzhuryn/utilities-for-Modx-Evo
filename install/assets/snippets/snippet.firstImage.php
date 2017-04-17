e6dd8a6b090142a71f7b8ba4c0521b05ae804fcf
dzhuryn <dzhuryn.volodymyr@gmail.com>
2017-04-17 18:51:25 +0300

    test

--- install/assets/snippets/snippet.firstImage.php
+++ install/assets/snippets/snippet.firstImage.php
@@ -2,15 +2,14 @@
 /**
  * firstImage
  *
- * Вывод первой картинки
+ * Первая картинка из multiTV
  *
- * @category 	snippet
- * @version 	0.1
- * @license 	http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
- * @internal	@modx_category sys
- * @internal    @legacy_names firstImage
- * @internal   	@installset base, sample
+ * @category 	   snippet
+ * @version 	   0.1
+ * @license 	   http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
+ * @internal	   @properties
+ * @internal	   @modx_category sys
+ * @internal   	   @installset base, sample
  */
-
 
 require MODX_BASE_PATH.'assets/snippets/firstImage/firstImage.inc.php';
