dd82c9e3060d80a05b09f907a357dfe9ca71c08f
dzhuryn <dzhuryn.volodymyr@gmail.com>
2017-07-05 22:11:52 +0300

    add query parser

--- assets/snippets/default/default.inc.php
+++ assets/snippets/default/default.inc.php
@@ -2,6 +2,8 @@
 $val = isset($val)?$val:'';
 $name = isset($name)?$name:'';
 $data = [];
+
+
 if(empty($GLOBALS['default'][$name])){
     $T = $modx->getFullTableName('site_tmplvars');
     $tvName = $modx->db->escape($name);
@@ -27,15 +29,24 @@ if(empty($GLOBALS['default'][$name])){
             }
         }
     }
+    if (stristr($element, "@SELECT")) {
+        $out = [];
+        $element = str_replace(['@SELECT','[+PREFIX+]'],['SELECT',$modx->db->config['table_prefix']],$element);
+        $resp = $modx->db->makeArray($modx->db->query($element));
+        foreach ($resp as $el) {
+            $keys = array_keys($el);
+            $out[$el[$keys[1]]] = $el[$keys[0]];
+        }
+    }
     $GLOBALS['default'][$name] = $out;
-
     $data = $out;
 
 }
 else{
     $data = $GLOBALS['default'][$name];
 }
+$output = '';
 if(!empty($data[$val])){
-    $val = $data[$val];
+    $output = $data[$val];
 }
-echo $val;
+echo $output;
